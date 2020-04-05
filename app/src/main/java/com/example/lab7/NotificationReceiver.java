44adecba93809a2cdd47903701cbf8e54f0d2a6b
Ryan Martinez <RyanM0126@github.com>
2020-04-05 16:57:08 -0500

    milestone2

--- app/src/main/java/com/example/lab7/NotificationReceiver.java
+++ app/src/main/java/com/example/lab7/NotificationReceiver.java
@@ -1,4 +1,15 @@
 package com.example.lab7;
 
-public class NotificationReceiver {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class NotificationReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String message = intent.getStringExtra("toastMessage");
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
 }
